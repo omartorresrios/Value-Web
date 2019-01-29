26ae485b1f993a5a0f64b1229e025176f4ee462f
Omar Torres <torresomar44@gmail.com>
2019-01-29 18:22:22 -0500

    Add Skeleton CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Link } from 'react-router-dom';
+import '../styles/vendors/normalize.css';
+import '../styles/vendors/skeleton.css';
 import './App.css';
 
 class App extends Component {
