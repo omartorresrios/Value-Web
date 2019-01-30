2458d704f0804ba5a6bf4a84b01fed29681a789b
Omar Torres <torresomar44@gmail.com>
2019-01-29 20:41:35 -0500

    Create Header component

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 const MainLayout = (props) => {
   return (
     <div className="main-layout">
+      <Header />
       <div className="container">
         Hola pendejos
         {props.children}
