62d07e07a04ccb72dcbf7c5747b684ce45d9703d
Omar Torres <torresomar44@gmail.com>
2019-01-29 21:20:41 -0500

    Replace hashHistory with browserHistory for cleaner URL

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, browserHistory } from 'react-router-dom';
 
 import MainLayout from '../components/MainLayout';
 import App from '../components/App';
 import Profile from '../components/Profile';
 
 export default (
-  <BrowserRouter>
+  <BrowserRouter history={browserHistory}>
     <div>
       <MainLayout>
         <Route exact component={App}/>
