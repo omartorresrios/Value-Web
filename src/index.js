987fb54d45171adf9cac8cbd9cfc41907c240317
Omar Torres <torresomar44@gmail.com>
2019-01-30 12:29:15 -0500

    Install react-router-redux to keep route in sync with redux state

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import configureStore from './store/configureStore';
-import routes from './routes';
+import createRoutes from './routes';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 const store = configureStore();
+const routes = createRoutes(store);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 import { browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 
 import MainLayout from '../components/MainLayout';
 import Home from '../components/Home';
 import Profile from '../components/Profile';
 
-export default (
-  <BrowserRouter history={browserHistory}>
-    <div>
-      <MainLayout>
-        <Route exact component={Home}/>
-        <Route path="/profile" component={Profile}/>
-      </MainLayout>
-    </div>
-  </BrowserRouter>
-);
+const createRoutes = (store) => {
+  // Create an enhanced history that syncs navigation events with the store
+  const history = syncHistoryWithStore(browserHistory, store)
+  return (
+    <BrowserRouter history={browserHistory}>
+      <div>
+        <MainLayout>
+          <Route exact component={Home}/>
+          <Route path="/profile" component={Profile}/>
+        </MainLayout>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default createRoutes;
